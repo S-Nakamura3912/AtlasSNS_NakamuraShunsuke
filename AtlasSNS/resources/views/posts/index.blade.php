59fff2f502ed1519592864e7ecd3890e45ee5599
中村駿佑 <>
2025-07-15 18:46:13 +0900

    投稿フォーム設置

--- AtlasSNS/resources/views/posts/index.blade.php
+++ AtlasSNS/resources/views/posts/index.blade.php
@@ -2,5 +2,26 @@
 
 @section('content')
 <h2>機能を実装していきましょう。</h2>
+<div class="container">
+  {!! Form::open(['url' => '/post', 'method' => 'POST']) !!}
+
+  <div class="form-group">
+    {{ Form::input('text', 'authorName', null, ['required', 'class' => 'form-control', 'placeholder' => '投稿内容を入力してください']) }}
+  </div>
+
+  <button type="submit" class="btn btn-success pull-right">
+    <img src="{{ asset('images/post.png') }}" alt="送信">
+  </button>
+
+  {!! Form::close() !!}
+</div>
+
+
+
+
+
+
+
+
 
 @endsection
